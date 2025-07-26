27c51141b32397cccfa8009ec42420990e395f52
X-Zero-L <x-zero-l@users.noreply.github.com>
2025-07-26 19:31:17 +0000

    update

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -14,6 +14,21 @@ import LanguageSelector from '@/components/LanguageSelector.vue'
 const currentLangCode = ref('en-US')
 const isDark = ref(false)
 const appStore = useAppStore()
+const activeTab = ref('info')
+
+const tabs = [
+  { id: 'info', label: 'Network Information', icon: 'M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9' },
+  { id: 'tools', label: 'Network Tools', icon: 'M15 12a3 3 0 11-6 0 3 3 0 016 0zM2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z' },
+  { id: 'speedtest', label: 'Speed Test', icon: 'M13 10V3L4 14h7v7l9-11h-7z' },
+  { id: 'traffic', label: 'Traffic Monitor', icon: 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z' }
+]
+
+const filteredTabs = computed(() => {
+  if (!appStore.config.feature_iface_traffic) {
+    return tabs.filter(tab => tab.id !== 'traffic')
+  }
+  return tabs
+})
 
 const currentLang = computed(() => {
   for (const lang of langList) {
